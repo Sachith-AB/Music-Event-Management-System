831908d3fdfcbfa1ea090f8567d0adca81d9219c
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-23 10:35:29 +0530

    complete menu and footer

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <title>Event Management System-Home Page</title>
+    <link rel="stylesheet" href="./css/admin.css">
+    <body>
+        <!--Header -->
+        <div class="menu">
+            <div class="wrapper">
+                <ul class="">
+                    <li><a href="home">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--Header end -->
+        
+        <!-- Main contanent -->
+        <div>
+            <div class="wrapper">
+                Main
+            </div>
+        </div>
+        <!--Main end -->
+        <!--Footer-->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">This is footer</p>
+            </div>
+        </div>
+        <!--Footer end -->
+
+    </body>
+</html>
