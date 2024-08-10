5d07f2a080532d5c0aac878012c73c2ac3b63b59
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-08-10 15:15:50 +0530

    display home class

--- app/core/App.php
+++ app/core/App.php
@@ -1,17 +1,19 @@
 <?php
-function splitUrl(){
-    $URL = $_GET['url'] ?? 'home';
-    $URL = explode("/", $URL);
-    return $URL;
-}
-
-function loadController(){
-    $URL = splitUrl();
-    $fileName = "../app/controllers/".ucfirst($URL[0]).".php";
-    if(file_exists($fileName)){
-        require $fileName;
-    }else{
-        $fileName = "../app/controllers/_404.php";
-        require $fileName;
+class App{
+    private function splitUrl(){
+        $URL = $_GET['url'] ?? 'home';
+        $URL = explode("/", $URL);
+        return $URL;
+    }
+    
+    public function loadController(){
+        $URL = $this->splitUrl();
+        $fileName = "../app/controllers/".ucfirst($URL[0]).".php";
+        if(file_exists($fileName)){
+            require $fileName;
+        }else{
+            $fileName = "../app/controllers/_404.php";
+            require $fileName;
+        }
     }
 }
