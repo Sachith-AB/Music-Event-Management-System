c62f4e3392ca5a2cb978efd4df01ce6bf5ebacd9
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-08-15 10:39:45 +0530

    create sign up functionlites

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -6,6 +6,24 @@ class Signup {
     use Model;
 
     public function index() {
-        $this->view('signup');
+
+        $data = [];
+
+        
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $user = new User;
+            if($user->validUser($_POST)){
+                $user->insert($_POST);
+                redirect('signin');
+            }else {
+                $data['errors'] = $user->errors;
+            }
+    
+        }
+        
+        
+        $this->view('signup',$data);
     }
 }
--- app/views/signup.view.php
+++ app/views/signup.view.php
@@ -10,30 +10,35 @@
     <div class="container">
         <div class="form-box">
             <h1>Get Started</h1>
-            <form>
+            <form method="POST">
                 <div class="input-wrap">
                     <label for="name">Name</label>
-                    <input type="text" id="name" placeholder="Name" required>
+                    <input name="name" type="text" id="name" placeholder="Name" >
 
                 </div>
                 <label for="email">Email</label>
-                <input type="email" id="email" placeholder="Email" required>
+                <input name="email" type="email" id="email" placeholder="Email" >
 
                 <label for="password">Password</label>
-                <input type="password" id="password" placeholder="Password" required>
+                <input name="password" type="password" id="password" placeholder="Password" >
                 
                 <label for="password">Confirm Password</label>
-                <input type="password" id="password" placeholder="Confirm Password" required>
+                <input name="confirm-password" type="password" id="confirm password" placeholder="Confirm Password" >
 
                 <div class="terms">
-                    <input type="checkbox" id="agree" required>
+                    <input name="agree" type="checkbox" id="agree">
                     <label for="agree">I agree with&nbsp;<a href="#">Terms & Conditions</a></label>
-
                 </div>
                 
                 <button type="submit" class="sign-up-btn">Sign Up</button>
                 
                 <p class="sign-in-link">Already have an account? <a href="signin">Log in</a></p>
+
+                <?php if(!empty($errors)): ?>
+                    <div>
+                        <?=implode("<br>",$errors)?>
+                    </div>
+                <?php endif; ?>
             </form>
         </div>
     </div>
