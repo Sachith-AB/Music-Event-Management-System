9cf8be70dd8bcfa5e4c77632ff4cfc665f2c6f22
thevindu02 <dil.thevi@gmail.com>
2024-08-15 12:48:21 +0530

    create UI

---
+++ app/views/request/request.view.php
@@ -0,0 +1,168 @@
+<html>
+
+<head>
+
+    <title>Request</title>
+
+    <link rel="stylesheet" href="eventReq.css">
+
+</head>
+
+<div class = "head">
+
+    <ul>
+
+        <li ><a href="#">Home</a></li>
+        <li><a href="#">Request</a></li>
+        <li><a href="#">Calender</a>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Payment Info</a></li>
+    
+    </ul>
+        
+</div>
+
+<div class = "input" >
+
+    <input class = "search" type="text" placeholder = "Search...">  
+
+</div>
+
+<div class = "main-content" >
+
+
+    <h1>REQUESTS</h1>
+
+    
+    <div class = "container">
+
+        <div class = "item">
+
+                <img src="https://th.bing.com/th/id/OIP.gx8w1GUUL5uclnleK93M-gHaE9?w=265&h=180&c=7&r=0&o=5&pid=1.7" alt="">
+                <h4>JAZZ Night</h4>
+                
+                <p>Date: Oct 20,2024</p>
+                <p>Location: BlueNote,NY</p>
+
+                <button class = "details">
+                    View Details
+                </button>
+  
+                <div class = "confirm">
+
+                    <button class = "left" > 
+                        Accept
+                    </button>
+
+                    <button class = "right" >
+                        Reject
+                    </button>
+
+
+                </div>
+        </div>
+
+        <div class = "item">
+                <img src="https://th.bing.com/th/id/OIP.iRaLGvQGVuVVrX0kAmc9bgHaE8?w=286&h=189&c=7&r=0&o=5&pid=1.7" alt="">
+                <h4>Rock Fest</h4>
+                <p>Date: Nov 5,2023</p>
+                <p>Location: Madison Square,NY</p>
+
+                <button class = "details">
+                    View Details
+                </button>
+
+
+            <div class = "confirm">
+
+                    <button class = "left" > 
+                        Accept
+                    </button>
+
+                    <button class = "right" >
+                        Reject
+                    </button>
+                    
+
+            </div>
+                    
+        </div>
+
+        <div class = "item">
+    
+            <img src="https://th.bing.com/th/id/OIP._eE2-4mf20TRcIsNqz1EbwHaEK?w=304&h=180&c=7&r=0&o=5&pid=1.7" alt="">
+            <h4>Classical Evening</h4>
+            <p>Date: Dec,13,2024</p>
+            <p>Location: Carneage Hall,NY</p>
+
+            <button class = "details">
+                    View Details
+            </button>
+
+            <div class = "confirm">
+
+                    <button class = "left" > 
+                        Accept
+                    </button>
+
+                    <button class = "right" >
+                        Reject
+                    </button>
+                    
+
+            </div>
+
+        </div>
+            
+    </div>
+
+    
+</div>
+
+<div class = "rest">
+
+    <h1>ACCEPTED EVENTS</h1>
+
+    <input type="text" placeholder = "Sort by Date...">
+    
+    <div class="container" >
+
+            <div class = "item2">
+                
+
+                <div class = "info" >
+
+                    <h4>JAZZ Night</h4>
+                    <p>Date: 25th October 2024</p>
+                    <p>Venue: BlueNote,Jazz Club,NY</p>
+                    <button>details</button>
+
+                </div>
+
+                <img src="https://th.bing.com/th/id/OIP.gx8w1GUUL5uclnleK93M-gHaE9?w=265&h=180&c=7&r=0&o=5&pid=1.7" alt="">
+                
+            </div>
+
+            <div class = "item2" >
+
+                
+                <div class = "info" >
+                    <h4>Rock Fest</h4>
+                    <p>Date: November 5 2024</p>
+                    <p>Location: Madison Square,NY</p>
+                    <button>details</button>
+                </div>
+                
+                <img src="https://th.bing.com/th/id/OIP.iRaLGvQGVuVVrX0kAmc9bgHaE8?w=286&h=189&c=7&r=0&o=5&pid=1.7" alt="">
+
+
+            </div>
+
+    </div>
+    
+    
+</div>
+
+
+</html>
+
