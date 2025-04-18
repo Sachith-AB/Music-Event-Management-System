ae2e7b967047f913bdd76137eb81ccafbb285315
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-04-18 09:34:26 +0530

    complete UI perfectly in reset password page

--- app/views/forgotPassword/resetpassword.view.php
+++ app/views/forgotPassword/resetpassword.view.php
@@ -5,24 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/forgotpassword/resetpassword.css">
+    
 </head>
 <body>
     <div class="password-reset-container">
         <h2>Set New Password</h2>
         <p>Enter your new password below</p>
         <form method="POST">
-        <div class="input-wrap">
-                <input type="text" name="n-password" class="input-field" id="n-password">
-                <label for="new-password">New Password</label>
-                <a href="#" class="hide active" onclick="togglePasswordVisibility('n-password','s-toggleIcon')">
+            <div class="input-wrap">
+                <input type="password" name="password" class="input-field" id="s-password" >
+                <label for="pass">Password</label>
+                <a href="#" class="hide active" onclick="togglePasswordVisibility('s-password','s-toggleIcon')">
                     <ion-icon name="eye-outline" id="s-toggleIcon"></ion-icon>
                 </a>
             </div>
             <div class="input-wrap">
-                <input type="text" name="c-password" class="input-field" id="c-password">
-                <label for="confirm-password">Confirm Password</label>
-                <a href="#" class="hide active" onclick="togglePasswordVisibility('c-password','s-toggleIcon')">
-                    <ion-icon name="eye-outline" id="s-toggleIcon"></ion-icon>
+                <input type="password" name="confirm-password" class="input-field" id="c-password" >
+                <label for="pass">Confirm Password</label>
+                <a href="#" class="hide active" onclick="togglePasswordVisibility('c-password','c-toggleIcon')">
+                    <ion-icon name="eye-outline" id="c-toggleIcon"></ion-icon>
                 </a>
             </div>
             <button type="submit">Reset Password</button>
