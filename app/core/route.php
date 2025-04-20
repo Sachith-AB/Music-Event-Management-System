4c37be37b15ac2adc43b29fd33fa1d5334db56b1
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-04-20 19:23:04 +0530

    create restrict link depend on user role

--- app/core/route.php
+++ app/core/route.php
@@ -28,47 +28,62 @@ route('event-report','event/Report@index');
 route('event-payment','event/EventPayment@index');
 route('event-view-payment','event/ViewEventPayment@index');
 
-
-//route for event collaborators
-route('colloborator-profile','eventCollaborator/singerProfile@index');
-route('colloborator-dashboard','eventCollaborator/SingerDashboard@index');
-route('colloborator-request','eventCollaborator/SingerRequest@index');
-route('colloborator-updateprofile','eventCollaborator/SingerUpdateProf@index');
-route('colloborator-calendar','eventCollaborator/Calender@index');
-route('colloborator-events','eventCollaborator/SingerEvents@index');
-route('colloborator-payments','eventCollaborator/SingerPayments@index');
-route('collaborator-viewprofile','eventCollaborator/ViewProfile@index');
-route('collaborator-eventdetails','eventCollaborator/EventDetails@index');
-route('collaborator-report','eventCollaborator/collaboratorReport@index');
-route('send-message','eventCollaborator/Chat@sendMessage');
-route('get-messages','eventCollaborator/Chat@getMessages');
-
-
-
+if($_SESSION['USER']->role == 'collaborator' && $_SESSION['USER']->role == 'admin'){
+    //route for event collaborators
+    route('colloborator-profile','eventCollaborator/singerProfile@index');
+    route('colloborator-dashboard','eventCollaborator/SingerDashboard@index');
+    route('colloborator-request','eventCollaborator/SingerRequest@index');
+    route('colloborator-updateprofile','eventCollaborator/SingerUpdateProf@index');
+    route('colloborator-calendar','eventCollaborator/Calender@index');
+    route('colloborator-events','eventCollaborator/SingerEvents@index');
+    route('colloborator-payments','eventCollaborator/SingerPayments@index');
+    route('collaborator-viewprofile','eventCollaborator/ViewProfile@index');
+    route('collaborator-eventdetails','eventCollaborator/EventDetails@index');
+    route('collaborator-report','eventCollaborator/collaboratorReport@index');
+    route('send-message','eventCollaborator/Chat@sendMessage');
+    route('get-messages','eventCollaborator/Chat@getMessages');
+}
 
 
 
+//all routes for event planner
+if ($_SESSION['USER']->role == 'planner' && $_SESSION['USER']->role == 'admin'){
+    route('request','request/Requestview@index');
 
-//route for requestlist
-route('request','request/Requestview@index');
+    //route for request
+    route('request-singers','request/SingerRequest@index');
+    route('request-bands','request/BandRequest@index');
+    route('request-sounds','request/SoundRequest@index');
+    route('request-decorators','request/DecoratorsRequest@index');
+    route('request-stages','request/StageRequest@index');
+    route('request-announcers','request/AnnouncerRequest@index');
+    route('request-venues','request/VenueRequest@index');
 
-//route for request
-route('request-singers','request/SingerRequest@index');
-route('request-bands','request/BandRequest@index');
-route('request-sounds','request/SoundRequest@index');
-route('request-decorators','request/DecoratorsRequest@index');
-route('request-stages','request/StageRequest@index');
-route('request-announcers','request/AnnouncerRequest@index');
-route('request-venues','request/VenueRequest@index');
+    
+    //Routes for Ticket
+    route('create-ticket', 'ticket/TicketController@index');
+    route('view-tickets', 'ticket/TicketController@viewTickets');
+    route('update-ticket', 'ticket/TicketController@updateticket');
+    route('delete-ticket', 'ticket/TicketController@deleteTicket');
+
+    // Route for event planners
+    route('event-planner-profile','eventPlanner/Profile@index');
+    route('event-planner-dashboard','eventPlanner/EventPlannerDashboard@index');
+    route('event-planner-calendar','eventPlanner/EventPlannerCalendar@index');
+    route('event-planner-myevents','eventPlanner/EventPlannerMyEvents@index');
+    route('event-planner-payment','eventPlanner/EventPlannerPayment@index');
+    route('event-planner-messages','eventPlanner/EventPlannerMessage@index');
+    route('event-planner-viewEvent','eventPlanner/EventPlannerViewEvent@index');
+    route('event-planner-scheduledEvent','eventPlanner/EventPlannerScheduledEvent@index');
+    route('get-event-messages','eventPlanner/EventPlannerMessage@index');
+    route('collaborator-payment','eventPlanner/CollaboratorPayment@index');
+    route('event-planner-completedEvent','eventPlanner/EventPlannerCompletedEvent@index');
+    route('event-planner-completedEventInfo','eventPlanner/EventPlannerCompletedEventInfo@index');
+    route('calender','calender/Calender@index');
+}
 
 
-route('calender','calender/Calender@index');
 
-//Routes for Ticket
-route('create-ticket', 'ticket/TicketController@index');
-route('view-tickets', 'ticket/TicketController@viewTickets');
-route('update-ticket', 'ticket/TicketController@updateticket');
-route('delete-ticket', 'ticket/TicketController@deleteTicket');
 
 
 //Route for purchaseticket
@@ -78,38 +93,26 @@ route('successfullypaid','ticket/Successfullypaid@index');
 route('upcommingevent','ticket/UpcomingEvents@index');
 route('ticketevent','ticket/PurchaseTicket@index4');
 
+if($_SESSION['USER']->role == 'planner' && $_SESSION['USER']->role == 'admin'){
+    // Route for ticket holder
+    route('profile','ticketHolder/Profile@index');
+    route('update-profile','ticketHolder/UpdateProf@index');
+    route('ticket-holder-home','ticketHolder/TicketHolderHome@index');
+    route('view-pastevent','ticketHolder/ViewPastevent@index');
+    route('notification-event','ticketHolder/NotificationEvent@index');
+    route('profile/markread','ticketHolder/ViewPastevent@markNotificationsRead');
+}
+
+if($_SESSION['USER']->role == 'admin'){
+    //Route for Admin
+    route('admin-dashboard','admin/AdminDashboard@index');
+    route('admin-eventplanners','admin/EventPlanners@index');
+    route('admin-eventcollaborators','admin/EventCollaborators@index');
+    route('admin-ticketholders','admin/TicketHolders@index');
+    route('admin-vieweventplanner','admin/ViewEventPlanner@index');
+    route('admin-event-report','admin/AdminEventReport@index');
+    route('admin-ticket-report','admin/AdminTicketReport@index');
+    route('admin-user-report','admin/AdminUserReport@index');
+}
 
 
-// Route for ticket holder
-route('profile','ticketHolder/Profile@index');
-route('update-profile','ticketHolder/UpdateProf@index');
-route('ticket-holder-home','ticketHolder/TicketHolderHome@index');
-route('view-pastevent','ticketHolder/ViewPastevent@index');
-route('notification-event','ticketHolder/NotificationEvent@index');
-route('profile/markread','ticketHolder/ViewPastevent@markNotificationsRead');
-
-// Route for event planners
-route('event-planner-profile','eventPlanner/Profile@index');
-route('event-planner-dashboard','eventPlanner/EventPlannerDashboard@index');
-route('event-planner-calendar','eventPlanner/EventPlannerCalendar@index');
-route('event-planner-myevents','eventPlanner/EventPlannerMyEvents@index');
-route('event-planner-payment','eventPlanner/EventPlannerPayment@index');
-route('event-planner-messages','eventPlanner/EventPlannerMessage@index');
-route('event-planner-viewEvent','eventPlanner/EventPlannerViewEvent@index');
-route('event-planner-scheduledEvent','eventPlanner/EventPlannerScheduledEvent@index');
-route('get-event-messages','eventPlanner/EventPlannerMessage@index');
-route('collaborator-payment','eventPlanner/CollaboratorPayment@index');
-route('event-planner-completedEvent','eventPlanner/EventPlannerCompletedEvent@index');
-route('event-planner-completedEventInfo','eventPlanner/EventPlannerCompletedEventInfo@index');
-
-
-
-//Route for Admin
-route('admin-dashboard','admin/AdminDashboard@index');
-route('admin-eventplanners','admin/EventPlanners@index');
-route('admin-eventcollaborators','admin/EventCollaborators@index');
-route('admin-ticketholders','admin/TicketHolders@index');
-route('admin-vieweventplanner','admin/ViewEventPlanner@index');
-route('admin-event-report','admin/AdminEventReport@index');
-route('admin-ticket-report','admin/AdminTicketReport@index');
-route('admin-user-report','admin/AdminUserReport@index');
