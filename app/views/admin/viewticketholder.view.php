92faac29d14e916ff81d8425981be4d3e8ddabc9
irumi <irumiabeywickrama@gmail.com>
2025-04-22 10:53:25 +0530

    complete view ticket holder part

--- app/views/admin/viewticketholder.view.php
+++ app/views/admin/viewticketholder.view.php
@@ -27,13 +27,11 @@
     ?>
     <div class="page-content">
         <div class="all">
-        <div class="container">
+            <div class="container">
                 <h2>My Profile</h2>
-                
-                
-                    <div class="avatar">
-                        <img src="<?=ROOT?>/assets/images/user/<?php echo $_SESSION['USER']->pro_pic ?>" alt="user image">        
-                    </div>
+                <div class="avatar">
+                    <img src="<?=ROOT?>/assets/images/user/<?php echo $_SESSION['USER']->pro_pic ?>" alt="user image">        
+                </div>
                 <div class="details">
                     <h2 class="head2"><?php echo $_SESSION['USER']->name ?></h2>
                     <h3 class="head3"><?php echo $_SESSION['USER']->email ?></h3>
@@ -43,6 +41,57 @@
                     <div class="tag-item"><?= htmlspecialchars($ticketcount[0]) ?> events</div>
                     <div class="tag-item"><?= htmlspecialchars($ticketcount[1]) ?> tickets</div>
                     <div class="tag-item"><?= htmlspecialchars($ticketcount[2]) ?> price</div>
+                </div>
+            </div>
+            <div class="upcommingeve-tickets">
+                <div class="content">
+                    <h2 class="content-header">Past Events</h2>
+                    <div class="events-container">
+                        <?php foreach ($pastTickets as $event): ?>
+                            
+                            <div class="event-card">
+                                <a href="<?=ROOT?>/event-planner-viewEvent?id=<?= htmlspecialchars($event[0]->id) ?>" class="event-card-link">
+                                    
+                                    <img src="<?=ROOT?>/assets/images/events/<?= htmlspecialchars($event[0]->event_images) ?>" alt="<?= htmlspecialchars($event[0]->event_name) ?>">
+                                    <div>
+                                        <div><?= htmlspecialchars($event[0]->event_name) ?></div>
+                                        <div>
+                                            <div>📅 <?= htmlspecialchars(date("l, F d | h:i A", strtotime($event[0]->event_date))) ?></div>
+                                            <div class="two-line-ellipsis">📍 <?= htmlspecialchars($event[0]->address)?></div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                                
+                            
+                        <?php endforeach; ?>
+                    </div>
+
+
+                    <!-- futureevents -->
+                    <h2 class="content-header">Future Events</h2>
+                    <div class="events-container">
+                        <?php if (!empty($upcomingTickets)): ?>
+                            <?php foreach ($upcomingTickets as $event): ?>
+                                
+                                <div class="event-card">
+                                    <a href="<?=ROOT?>/event-planner-viewEvent?id=<?= htmlspecialchars($event[0]->id) ?>" class="event-card-link">
+                                        
+                                        <img src="<?=ROOT?>/assets/images/events/<?= htmlspecialchars($event[0]->event_images) ?>" alt="<?= htmlspecialchars($event[0]->event_name) ?>">
+                                        <div>
+                                            <div><?= htmlspecialchars($event[0]->event_name) ?></div>
+                                            <div>
+                                                <div>📅 <?= htmlspecialchars(date("l, F d | h:i A", strtotime($event[0]->event_date))) ?></div>
+                                                <div class="two-line-ellipsis">📍 <?= htmlspecialchars($event[0]->address)?></div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                    
+                                
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
 
