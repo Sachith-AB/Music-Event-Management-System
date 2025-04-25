b84bd5a2a16860b7c19ea470002ca39a78182566
irumi <irumiabeywickrama@gmail.com>
2025-04-25 09:46:11 +0530

    Add send notification to all the event collaborators

--- app/controllers/request/StageRequest.php
+++ app/controllers/request/StageRequest.php
@@ -7,11 +7,15 @@ class StageRequest {
     public function index(){
 
         $request = new Request;
+        $event = new Event;
+        $notificaton = new Notification;
+
         $data = [];
 
         if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['request'])) {
 
             $this->createRequest($request);
+            $this->createNotification($event,$notificaton,$_POST);
             
         }
 
@@ -90,6 +94,19 @@ class StageRequest {
     }
 
 
+    public function createNotification($event,$notification,$post){
+        $eventDetails = $event->firstById($post['event_id']);
+        $changes[] = "Event name: '{$eventDetails->event_name}' Event Date: '{$eventDetails->eventDate}'";
+        $link = "colloborator-request";
+        $notifymsg = [
+            'user_id' => $post['collaborator_id'],
+            'title' => "Recieved a request",
+            'message' => json_encode($changes),
+            'is_read' => 0,
+            'link' => $link,
+        ];
+        $notification->insert($notifymsg);
+    }
 
 
 }
