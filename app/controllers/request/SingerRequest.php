4ab573441b119bd20a9b7a6f90eef010b2d51d36
irumi <irumiabeywickrama@gmail.com>
2025-04-27 10:15:29 +0530

    add images for events

--- app/controllers/request/SingerRequest.php
+++ app/controllers/request/SingerRequest.php
@@ -9,6 +9,7 @@ class SingerRequest {
         $request = new Request;
         $calendar = new Calendar;
         $event = new Event;
+        $notification = new Notification;
         $data = [];
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['request'])) {
@@ -23,6 +24,7 @@ class SingerRequest {
                 redirect('request-singers?id=' . $_POST['event_id'] . '&' . $errorParams);
             
             } else {
+                $this->createNotification($event,$notification,$_POST);
                 $this->createRequest($request);
             }
         }
@@ -92,6 +94,19 @@ class SingerRequest {
 
     private function checkAvailability($calendar, $user_id, $event_date) {
         return $calendar->getAvailability($user_id, $event_date);
-    }    
+    }  
+    public function createNotification($event,$notification,$post){
+        $eventDetails = $event->firstById($post['event_id']);
+        $changes[] = "Event name: '{$eventDetails->event_name}' Event Date: '{$eventDetails->eventDate}'";
+        $link = "colloborator-request";
+        $notifymsg = [
+            'user_id' => $post['collaborator_id'],
+            'title' => "Recieved a request",
+            'message' => json_encode($changes),
+            'is_read' => 0,
+            'link' => $link,
+        ];
+        $notification->insert($notifymsg);
+    }  
 
 }
