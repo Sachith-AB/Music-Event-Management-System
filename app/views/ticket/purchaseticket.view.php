92bae830caa5e8fe61e7f4e3f3efed4914ef8fb0
irumi <irumiabeywickrama@gmail.com>
2025-04-28 13:57:05 +0530

    add note facility for event collaborator

--- app/views/ticket/purchaseticket.view.php
+++ app/views/ticket/purchaseticket.view.php
@@ -84,10 +84,10 @@
                             <input type="tel" id="phone" name="phone" placeholder="07********">
                         </div>
                     </div>
-                    <div class="checkbox-group">
+                    <!-- <div class="checkbox-group">
                         <label><input type="checkbox" name="updates" checked> Keep me updated on this event</label>
                         <label><input type="checkbox" name="agree" checked> I agree with the <a href="#">Terms of Use</a> & <a href="#">Privacy Policy</a></label>
-                    </div>
+                    </div> -->
                 </div>
              
 
